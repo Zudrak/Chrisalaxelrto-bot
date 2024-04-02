c9e7f4c46657d296e594eedaacc3811c733024ef
Alberto Jahuey Moncada <albertojahueymoncada@gmail.com>
2024-04-01 22:08:34 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 dependencies {
     implementation("net.dv8tion:JDA:5.0.0-beta.17")
     //implementation("com.sedmelluq:lavaplayer:1.3.78")
-    implementation("dev.arbjerg:lavaplayer:727959e9f621fc457b3a5adafcfffb55fdeaa538-SNAPSHOT")
+    implementation("dev.arbjerg:lavaplayer:4232036d5b1634570af19077f2a8eeedbc583087-SNAPSHOT")
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 
     runtimeOnly("com.h2database:h2")

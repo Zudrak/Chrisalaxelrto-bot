eaaec3f2c5ee4911fba0b5503744376fd4aa4111
Alberto Jahuey Moncada <albertojahueymoncada@gmail.com>
2024-12-04 18:16:02 -0800

    Added PO Tokens

--- src/main/kotlin/dev/arbjerg/ukulele/audio/LavaplayerConfig.kt
+++ src/main/kotlin/dev/arbjerg/ukulele/audio/LavaplayerConfig.kt
@@ -5,6 +5,7 @@ import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers
 import dev.arbjerg.ukulele.config.BotProps
 import dev.lavalink.youtube.YoutubeAudioSourceManager
+import dev.lavalink.youtube.clients.Web
 import dev.lavalink.youtube.clients.WebEmbeddedWithThumbnail
 import dev.lavalink.youtube.clients.WebWithThumbnail
 import org.springframework.context.annotation.Bean
@@ -17,9 +18,11 @@ class LavaplayerConfig(var botProps: BotProps) {
     fun playerManager(): AudioPlayerManager {
         val apm = DefaultAudioPlayerManager()
 
-        val ytSourceManager: YoutubeAudioSourceManager = YoutubeAudioSourceManager(true, WebWithThumbnail(), WebEmbeddedWithThumbnail())
+        Web.setPoTokenAndVisitorData(botProps.poToken, botProps.visitorData);
 
+        val ytSourceManager: YoutubeAudioSourceManager = YoutubeAudioSourceManager(true, WebWithThumbnail(), WebEmbeddedWithThumbnail())
         ytSourceManager.useOauth2(botProps.ytAuthToken, true);
+
         apm.registerSourceManager(ytSourceManager)
         AudioSourceManagers.registerRemoteSources(apm, com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager::class.java)
         return apm

35e0b71e0b6348344328707c4a4ae224a5a7d945
Axel Lopez <axepoi@hotmail.com>
2025-02-11 22:37:07 -0600

    deprecate static replies

--- src/main/kotlin/dev/arbjerg/ukulele/jda/ReplyListener.kt
+++ src/main/kotlin/dev/arbjerg/ukulele/jda/ReplyListener.kt
@@ -17,33 +17,36 @@ class ReplyListener() : ListenerAdapter() {
     private val log: Logger = LoggerFactory.getLogger(EventHandler::class.java)
 
     override fun onMessageReceived(event: MessageReceivedEvent){
-        if (!event.author.isBot) {
-            val channel = event.channel
-            val message = event.message
+        //deprecated
+        if(false){
+            if (!event.author.isBot) {
+                val channel = event.channel
+                val message = event.message
 
-            val stickers = message.getStickers()
+                val stickers = message.getStickers()
 
-            if(stickers.isNotEmpty()){
-                log.info("sticker found, id: ${stickers[0].getId()}")
-                for(reply in replies.list) {
-                    if (reply.first.containsMatchIn(stickers[0].getId())) {
+                if(stickers.isNotEmpty()){
+                    log.info("sticker found, id: ${stickers[0].getId()}")
+                    for(reply in replies.list) {
+                        if (reply.first.containsMatchIn(stickers[0].getId())) {
+                            val selectedReply = reply.second[Random.nextInt(reply.second.size)].trim()
+                            log.info("Replying $selectedReply to sticker ${stickers[0].getId()} in ${message.contentRaw}")
+                            var msg = MessageCreateBuilder().addContent(selectedReply).setTTS(true).build()
+                            channel.sendMessage(msg).queue()
+                        }
+                    }
+                }
+
+                for(reply in replies.list){
+                    if(reply.first.containsMatchIn(message.contentRaw.toLowerCase())){
                         val selectedReply = reply.second[Random.nextInt(reply.second.size)].trim()
-                        log.info("Replying $selectedReply to sticker ${stickers[0].getId()} in ${message.contentRaw}")
+                        log.info("Replying $selectedReply to ${message.contentRaw}")
                         var msg = MessageCreateBuilder().addContent(selectedReply).setTTS(true).build()
                         channel.sendMessage(msg).queue()
                     }
                 }
             }
-
-            for(reply in replies.list){
-                if(reply.first.containsMatchIn(message.contentRaw.toLowerCase())){
-                    val selectedReply = reply.second[Random.nextInt(reply.second.size)].trim()
-                    log.info("Replying $selectedReply to ${message.contentRaw}")
-                    var msg = MessageCreateBuilder().addContent(selectedReply).setTTS(true).build()
-                    channel.sendMessage(msg).queue()
-                }
-            }
-    }
+        }
     }
 
 }

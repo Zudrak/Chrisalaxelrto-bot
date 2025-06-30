700ef855c9ff3ac355c29da73c1b7f21a93fb8f4
Alberto Jahuey Moncada <albertojahueymoncada@gmail.com>
2025-06-29 17:20:43 -0700

    Fixed outgoing connections?

--- Src/Chrisalaxelrto.MusicStreamer/start.sh
+++ Src/Chrisalaxelrto.MusicStreamer/start.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Start tailscaled in the background
-tailscaled --state-dir=mem: --socket=/var/run/tailscale/tailscaled.sock --socks5-server=localhost:1055 &
+tailscaled --state=mem: --socket=/var/run/tailscale/tailscaled.sock --socks5-server=localhost:1055 &
 sleep 2
 
 # Authenticate with Tailscale
